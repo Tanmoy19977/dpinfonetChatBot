ab599b13a8ea6207830ffb97cd96b9ef95440751
unknown <tanmoychakrabarti1997@gmail.com>
2023-01-18 23:37:48 +0530

    db connect4

--- index.js
+++ index.js
@@ -40,9 +40,7 @@ async function dynamo_db_con(phone_no, user_name,message_body){
     message_body: message_body
 });
 }
-const CyclicDb = require("@cyclic.sh/dynamodb");
-const db = CyclicDb("long-jade-grasshopper-capCyclicDB");
-const user = db.collection("user");
+
 
 async function dynamo_db_con_getter(){
   getter_item = await user.get("user_name");
@@ -68,6 +66,10 @@ app.post("/webhook",(req,res)=>{ //i want some
                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
                let from = body_param.entry[0].changes[0].value.messages[0].from; 
                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+               const CyclicDb = require("@cyclic.sh/dynamodb");
+               const db = CyclicDb("long-jade-grasshopper-capCyclicDB");
+               const user = db.collection("user");
 
                console.log("phone number "+phon_no_id);
                console.log("from "+from);
